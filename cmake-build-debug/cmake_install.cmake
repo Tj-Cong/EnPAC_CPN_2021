e22832e91895866c0b6c507579b858cd465ec588
zlailmm <894531319@qq.com>
2022-04-19 18:58:50 +0800

    remove t_order

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/leo/pdnet/project/EnPAC_CPN_2021
+# Install script for directory: /home/leo/project/EnPAC_CPN_2021
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/leo/pdnet/project/EnPAC_CPN_2021/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/leo/project/EnPAC_CPN_2021/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
